36a5be88ec8f4089872dbd3dd1c59ad02ab2181e
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-10-28 19:24:12 +0500

    added api route to fetch all products

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,28 @@
 import express from "express";
 const router = express.Router();
 import Product from "../models/products.js";
+import ProductImage from "../models/Images.js";
 router.get("/", async (req, res) => {
-  const result = await Product.find();
-  res.json(result);
+  try {
+    const products = await Product.find();
+    if (products.length > 0) {
+      const allProducts = await Promise.all(
+        products.map(async (product) => {
+          const productImage = await ProductImage.findById(product.imageId);
+          return {
+            ...product._doc,
+            images: productImage
+          };
+        })
+      );
+      res.status(200).json({ success: true, products: allProducts });
+    } else {
+      res.status(200).json({ success: true, products: [] });
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ success: false, msg: "Some Error on Server Occurred!" });
+  }
 });
-
 export default router;
