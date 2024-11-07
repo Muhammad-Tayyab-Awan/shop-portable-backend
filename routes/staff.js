755f9d715481f3fb3bec217bf5683c51867fa644
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-07 20:21:45 +0500

    added collections

--- routes/staff.js
+++ routes/staff.js
@@ -1,3 +1,6 @@
 import express from "express";
+import Staff from "../models/staff.js";
+import User from "../models/users.js";
+import Address from "../models/addresses.js";
 const router = express.Router();
 export default router;
