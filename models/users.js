98c019fc8e7fa33e20c296fda75c9f0b70c85ec2
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-07 09:29:55 +0500

    added users model

---
+++ models/users.js
@@ -0,0 +1,60 @@
+import mongoose, { Schema } from "mongoose";
+const usersSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female"]
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  dob: {
+    type: Date,
+    required: true
+  },
+  joinedOn: {
+    type: Date,
+    default: Date.now
+  },
+  homeAddress: {
+    country: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    postalCode: {
+      type: Number,
+      required: true
+    },
+    fullAddress: {
+      type: String,
+      required: true
+    }
+  }
+});
+const User = mongoose.model("user", usersSchema);
+export default User;
