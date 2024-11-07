9666f875faa99364bd3e10eded67b16ab06b47a0
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-07 09:38:35 +0500

    added addresses model

---
+++ models/addresses.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+const addressSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    unique: true,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  postalCode: {
+    type: Number,
+    required: true
+  },
+  fullAddress: {
+    type: String,
+    required: true
+  },
+  addedOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+const Addresses = mongoose.model("address", productSchema);
+export default Product;
