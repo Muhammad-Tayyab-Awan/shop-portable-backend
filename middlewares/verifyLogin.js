5d810b9fca869d69989b420f7554acad858f785c
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-08 12:40:53 +0500

    added middleware to verify login with token

---
+++ middlewares/verifyLogin.js
@@ -0,0 +1,23 @@
+import JWT from "jsonwebtoken";
+const JWT_SECRET = process.env.JWT_SECRET;
+async function verifyLogin(req, res, next) {
+  let token = req.header("auth-token");
+  if (!token) {
+    res
+      .status(401)
+      .json({ success: false, error: "Access denied from server!" });
+  } else {
+    JWT.verify(token, JWT_SECRET, (err, response) => {
+      if (err) {
+        res.status(403).json({
+          status: false,
+          error: "Token is not valid!"
+        });
+      } else {
+        req.staffId = response.id;
+        next();
+      }
+    });
+  }
+}
+export default verifyLogin;
