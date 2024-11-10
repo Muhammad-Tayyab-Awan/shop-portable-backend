a7f3a89b6786648cbe6fc1d9892de94350a3d190
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-10 13:37:33 +0500

    modified model

--- models/staff.js
+++ models/staff.js
@@ -1,73 +1,77 @@
 import mongoose, { Schema } from "mongoose";
-const staffSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  firstName: {
+const addressSchema = new Schema({
+  country: {
     type: String,
     required: true
   },
-  lastName: {
+  state: {
     type: String,
     required: true
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  emailVerified: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  role: {
-    type: String,
-    required: true,
-    enum: ["admin", "deliveryMan", "productsManager"]
-  },
-  password: {
+  city: {
     type: String,
     required: true
   },
-  gender: {
-    type: String,
-    required: true,
-    enum: ["Male", "Female"]
-  },
-  dob: {
-    type: Date,
+  postalCode: {
+    type: Number,
     required: true
   },
-  joinedOn: {
-    type: Date,
-    required: true,
-    default: Date.now
-  },
-  homeAddress: {
-    country: {
-      type: String,
-      required: true
-    },
-    state: {
-      type: String,
-      required: true
-    },
-    city: {
-      type: String,
-      required: true
-    },
-    postalCode: {
-      type: Number,
-      required: true
-    },
-    fullAddress: {
-      type: String,
-      required: true
-    }
+  fullAddress: {
+    type: String,
+    required: true
   }
 });
+const staffSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    emailVerified: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["admin", "deliveryMan", "productsManager"]
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["Male", "Female"]
+    },
+    dob: {
+      type: Date,
+      required: true
+    },
+    joinedOn: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    homeAddress: addressSchema
+  },
+  { timestamps: true }
+);
 const Staff = mongoose.model("staff", staffSchema);
 export default Staff;
