adc9781ab45464886b3505fdafc404e6d8e2fba0
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-12 18:29:13 +0500

    updated

--- models/profileImages.js
+++ models/profileImages.js
@@ -1,32 +1,31 @@
 import mongoose, { Schema } from "mongoose";
 import { v4 as uuidv4 } from "uuid";
-const profileImageSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
-    unique: true,
-    required: true
+const profileImageSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      unique: true,
+      required: true
+    },
+    image: {
+      type: Buffer,
+      unique: true,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true,
+      default: `profile_image_${uuidv4}`
+    },
+    contentType: {
+      type: String,
+      required: true,
+      default: "image/png",
+      enum: ["image/png"]
+    }
   },
-  image: {
-    type: Buffer,
-    unique: true,
-    required: true
-  },
-  alt: {
-    type: String,
-    required: true,
-    default: `profile_image_${uuidv4}`
-  },
-  contentType: {
-    type: String,
-    required: true,
-    default: "image/png",
-    enum: ["image/png"]
-  },
-  addedOn: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 const ProfileImage = mongoose.model("profileImage", profileImageSchema);
 export default ProfileImage;
