35c9f7557a4ec7fa2a58426599884ead87c0cf51
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-12 18:29:38 +0500

    added new model for staffProfileImage

---
+++ models/staffProfileImages.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+const staffProfileImageSchema = new Schema(
+  {
+    staffMember: {
+      type: Schema.Types.ObjectId,
+      ref: "staff",
+      unique: true,
+      required: true
+    },
+    image: {
+      type: Buffer,
+      unique: true,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true,
+      default: `profile_image_${uuidv4}`
+    },
+    contentType: {
+      type: String,
+      required: true,
+      default: "image/png",
+      enum: ["image/png"]
+    }
+  },
+  { timestamps: true }
+);
+const StaffProfileImage = mongoose.model(
+  "staffProfileImage",
+  staffProfileImageSchema
+);
+export default ProfileImage;
