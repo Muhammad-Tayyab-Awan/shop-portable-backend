83361b915768c57fcbe23dd151e8508949b5ecbf
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-16 18:55:31 +0500

    added admin and product manager login validator middleware

---
+++ middlewares/verifyAdPMLogin.js
@@ -0,0 +1,38 @@
+import JWT from "jsonwebtoken";
+import Staff from "../models/staff.js";
+const JWT_SECRET = process.env.JWT_SECRET;
+async function verifyAdPMLogin(req, res, next) {
+  let token = req.header("auth-token");
+  if (!token) {
+    res
+      .status(401)
+      .json({ success: false, error: "Access denied from server!" });
+  } else {
+    JWT.verify(token, JWT_SECRET, async (err, response) => {
+      if (err) {
+        res.status(403).json({
+          status: false,
+          error: "Token is not valid!"
+        });
+      } else {
+        const staffMember = await Staff.findById(response.id);
+        if (staffMember) {
+          if (
+            staffMember.role === "admin" ||
+            staffMember.role === "productsManager"
+          ) {
+            req.staffId = response.id;
+            next();
+          } else {
+            req
+              .status(401)
+              .json({ success: false, error: "Access Denied by Server" });
+          }
+        } else {
+          res.status(400).json({ success: false, error: "Token is Tempered" });
+        }
+      }
+    });
+  }
+}
+export default verifyAdPMLogin;
