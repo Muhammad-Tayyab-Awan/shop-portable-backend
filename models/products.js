3d73975909698473f8528bd261d309f658faacc8
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-16 19:55:30 +0500

    updated model

--- models/products.js
+++ models/products.js
@@ -1,58 +1,62 @@
 import mongoose, { Schema } from "mongoose";
-const productSchema = new Schema({
-  pname: {
-    type: String,
-    unique: true,
-    required: true
+const productSchema = new Schema(
+  {
+    pname: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    imageId: {
+      type: Schema.Types.ObjectId,
+      ref: "productimage",
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    category: {
+      type: String,
+      enum: ["Laptop", "Accessory"],
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ["Used", "New"],
+      default: "New"
+    },
+    stock: {
+      type: Number,
+      required: true
+    },
+    discount: {
+      type: Number,
+      default: 0.0
+    },
+    brand: {
+      type: String,
+      required: true
+    },
+    sold: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    productCreator: {
+      type: Schema.Types.ObjectId,
+      ref: "staff",
+      required: true
+    },
+    launchDate: {
+      type: Date,
+      default: Date.now
+    }
   },
-  imageId: {
-    type: Schema.Types.ObjectId,
-    ref: "productimage",
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  category: {
-    type: String,
-    enum: ["Laptop", "Accessory"],
-    required: true
-  },
-  status: {
-    type: String,
-    enum: ["Used", "New"],
-    default: "New"
-  },
-  stock: {
-    type: Number,
-    required: true
-  },
-  discount: {
-    type: Number,
-    default: 0.0
-  },
-  brand: {
-    type: String,
-    required: true
-  },
-  sold: {
-    type: Number,
-    required: true
-  },
-  productCreator: {
-    type: Schema.Types.ObjectId,
-    ref: "staff",
-    required: true
-  },
-  launchDate: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 const Product = mongoose.model("product", productSchema);
 export default Product;
