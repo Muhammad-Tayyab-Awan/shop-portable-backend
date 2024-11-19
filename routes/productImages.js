3011bb5bc040e4c8bc86581debe1a8bd98b07638
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-19 14:02:39 +0500

    added route to get images of a product onk\ly for admins and product managers

--- routes/productImages.js
+++ routes/productImages.js
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import verifyAdPMLogin from "../middlewares/verifyAdPMLogin.js";
 import ProductImage from "../models/images.js";
+import { param, validationResult } from "express-validator";
 const router = express.Router();
 router.route("/").get(verifyAdPMLogin, async (req, res) => {
   try {
@@ -17,4 +18,31 @@ router.route("/").get(verifyAdPMLogin, async (req, res) => {
     });
   }
 });
+router
+  .route("/:productId")
+  .get(verifyAdPMLogin, param("productId").isMongoId(), async (req, res) => {
+    try {
+      const result = validationResult(req);
+      if (result.isEmpty()) {
+        const productId = req.params.productId;
+        const productImages = await ProductImage.find({ product: productId });
+        if (productImages.length === 0) {
+          res.status(400).json({
+            success: false,
+            error: "No images found for that product"
+          });
+        } else {
+          res.status(200).json({ success: true, images: productImages });
+        }
+      } else {
+        res.status(400).json({ success: false, error: result.errors });
+      }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: "Error Occurred on Server Side",
+        message: error.message
+      });
+    }
+  });
 export default router;
