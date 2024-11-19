068967d76b62a92f10e841406baa4b9b7c8ce072
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-19 13:20:33 +0500

    changed product images model

--- models/images.js
+++ models/images.js
@@ -7,60 +7,21 @@ const imageSchema = new Schema({
     required: true,
     unique: true
   },
-  image1: {
+  image: {
     type: Buffer,
     unique: true,
     required: true
   },
-  image2: {
-    type: Buffer,
-    unique: true,
-    required: true
-  },
-  image3: {
-    type: Buffer,
-    unique: true,
-    required: true
-  },
-  image4: {
-    type: Buffer,
-    unique: true
-  },
-  image5: {
-    type: Buffer,
-    unique: true
-  },
-  alt1: {
+  alt: {
     type: String,
     required: true,
     default: `product_image_${uuidv4()}`
-  },
-  alt2: {
-    type: String,
-    required: true,
-    default: `product_image_${uuidv4()}`
-  },
-  alt3: {
-    type: String,
-    required: true,
-    default: `product_image_${uuidv4()}`
-  },
-  alt4: {
-    type: String
-  },
-  alt5: {
-    type: String
   },
   contentType: {
     type: String,
     required: true,
     default: "image/png",
     enum: ["image/png"]
-  },
-  imageCounts: {
-    type: Number,
-    required: true,
-    default: 3
   }
 });
 const ProductImage = mongoose.model("productimage", imageSchema);
