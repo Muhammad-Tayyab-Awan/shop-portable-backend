185a8760af9e5b5605bbdb5cb1569f7e487e81f7
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-20 18:49:34 +0500

    modified model

--- models/users.js
+++ models/users.js
@@ -1,67 +1,71 @@
 import mongoose, { Schema } from "mongoose";
-const usersSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  firstName: {
+const addressSchema = new Schema({
+  country: {
     type: String,
     required: true
   },
-  lastName: {
+  state: {
     type: String,
     required: true
   },
-  gender: {
-    type: String,
-    required: true,
-    enum: ["Male", "Female"]
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  emailVerified: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  password: {
+  city: {
     type: String,
     required: true
   },
-  dob: {
-    type: Date,
+  postalCode: {
+    type: Number,
     required: true
   },
-  joinedOn: {
-    type: Date,
-    default: Date.now
-  },
-  homeAddress: {
-    country: {
-      type: String,
-      required: true
-    },
-    state: {
-      type: String,
-      required: true
-    },
-    city: {
-      type: String,
-      required: true
-    },
-    postalCode: {
-      type: Number,
-      required: true
-    },
-    fullAddress: {
-      type: String,
-      required: true
-    }
+  fullAddress: {
+    type: String,
+    required: true
   }
 });
+const usersSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["Male", "Female"]
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    emailVerified: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    dob: {
+      type: Date,
+      required: true
+    },
+    joinedOn: {
+      type: Date,
+      default: Date.now
+    },
+    homeAddress: addressSchema
+  },
+  { timestamps: true }
+);
 const User = mongoose.model("user", usersSchema);
 export default User;
