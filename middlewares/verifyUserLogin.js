c8c2308213345215b3cceeda333853e921e82ef1
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-11-21 20:01:29 +0500

    added user login verification middleware

---
+++ middlewares/verifyUserLogin.js
@@ -0,0 +1,29 @@
+import JWT from "jsonwebtoken";
+import User from "../models/users";
+const JWT_SECRET = process.env.JWT_SECRET;
+async function verifyUserLogin(req, res, next) {
+  let token = req.header("auth-token");
+  if (!token) {
+    res
+      .status(401)
+      .json({ success: false, error: "Access denied from server!" });
+  } else {
+    JWT.verify(token, JWT_SECRET, async (err, response) => {
+      if (err) {
+        res.status(403).json({
+          status: false,
+          error: "Token is not valid!"
+        });
+      } else {
+        const user = await User.findById(response.id);
+        if (user) {
+          req.userId = response.id;
+          next();
+        } else {
+          res.status(400).json({ success: false, error: "Token is Tempered" });
+        }
+      }
+    });
+  }
+}
+export default verifyUserLogin;
