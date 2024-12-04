8905d951636568f7e7913c04965545aff42964d7
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-12-04 18:15:32 +0500

    added route to create new order

--- routes/order.js
+++ routes/order.js
@@ -1,25 +1,149 @@
 import express from "express";
 const router = express.Router();
 import Order from "../models/orders.js";
+import Address from "../models/addresses.js";
+import Product from "../models/products.js";
+import OrderItem from "../models/orderItems.js";
 import verifyUserLogin from "../middlewares/verifyUserLogin.js";
-import verifyAdminLogin from "../middlewares/verifyAdminLogin.js";
-router.route("/").get(verifyUserLogin, async (req, res) => {
-  try {
-    const userId = req.userId;
-    const allOrders = await Order.find({ user: userId });
-    if (allOrders.length > 0) {
-      res.status(200).json({ success: true, allOrders: allOrders });
-    } else {
-      res
-        .status(400)
-        .json({ success: false, error: "No orders found for current user" });
+import transporter from "../mailTransporter.js";
+import Staff from "../models/staff.js";
+import { body, validationResult } from "express-validator";
+import validator from "validator";
+router
+  .route("/")
+  .get(verifyUserLogin, async (req, res) => {
+    try {
+      const userId = req.userId;
+      const allOrders = await Order.find({ user: userId });
+      if (allOrders.length > 0) {
+        res.status(200).json({ success: true, allOrders: allOrders });
+      } else {
+        res
+          .status(400)
+          .json({ success: false, error: "No orders found for current user" });
+      }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: "Error Occurred on Server Side",
+        message: error.message
+      });
     }
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: "Error Occurred on Server Side",
-      message: error.message
-    });
-  }
-});
+  })
+  .post(
+    verifyUserLogin,
+    [
+      body("deliveryAddress").isMongoId(),
+      body("orderItems")
+        .isArray({ min: 1 })
+        .custom((orderItems) => {
+          for (const orderItem of orderItems) {
+            if (!validator.isMongoId(orderItem.productId)) {
+              throw new Error("Invalid MongoDB ID format in productId");
+            }
+            if (typeof orderItem.itemCount !== "number") {
+              throw new Error(
+                "Each product must have an itemCount of type number"
+              );
+            }
+          }
+          return true;
+        })
+    ],
+    async (req, res) => {
+      try {
+        const result = validationResult(req);
+        if (result.isEmpty()) {
+          const userId = req.userId;
+          let orderInfo = req.body;
+          const deliveryAddress = await Address.findById(
+            orderInfo.deliveryAddress
+          );
+          if (deliveryAddress && deliveryAddress.user.toString() === userId) {
+            let orderedItems = 0;
+            for (const orderItem of orderInfo.orderItems) {
+              const product = await Product.findById(orderItem.productId);
+              if (product) {
+                const availableCount = product.stock - product.sold;
+                if (
+                  availableCount >= orderItem.itemCount &&
+                  orderItem.itemCount > 0
+                ) {
+                  orderedItems++;
+                }
+              }
+            }
+            if (orderedItems === orderInfo.orderItems.length) {
+              const newOrder = await Order.create({
+                user: userId,
+                deliveryAddress: orderInfo.deliveryAddress
+              });
+              for (const orderItem of orderInfo.orderItems) {
+                const product = await Product.findById(orderItem.productId);
+                orderItem.orderId = newOrder.id;
+                orderItem.totalPrice =
+                  (product.price - (product.price * product.discount) / 100) *
+                  orderItem.itemCount;
+                await OrderItem.create(orderItem);
+                product.sold = product.sold + orderItem.itemCount;
+                await product.save();
+              }
+              const allOrderedItems = await OrderItem.find({
+                orderId: newOrder.id
+              });
+              let totalPrice = 0;
+              allOrderedItems.forEach((item) => {
+                totalPrice += item.totalPrice;
+              });
+              await Order.findByIdAndUpdate(newOrder.id, {
+                totalPrice: totalPrice
+              });
+              const admins = await Staff.find({
+                role: "admin",
+                emailVerified: true
+              }).select("email");
+              const htmlMessage = `<h1 style="text-align:center;">New Order</h1><p style="text-align:center;">Dear Admin! New Order with id <i>${newOrder.id}</i> created. Visit dashboard for further steps</p>`;
+              transporter.sendMail(
+                {
+                  to: admins,
+                  subject: "New Order",
+                  html: htmlMessage
+                },
+                (error) => {
+                  if (error) {
+                    res.status(500).json({
+                      success: false,
+                      error: "Error Occurred on Server Side",
+                      message: error.message
+                    });
+                  } else {
+                    res.status(200).json({
+                      success: true,
+                      error: "Your order created successfully"
+                    });
+                  }
+                }
+              );
+            } else {
+              res
+                .status(400)
+                .json({ success: false, error: "Invalid OrderItems Detail" });
+            }
+          } else {
+            res
+              .status(400)
+              .json({ success: false, error: "Invalid delivery address" });
+          }
+        } else {
+          res.status(200).json({ success: false, error: result.errors });
+        }
+      } catch (error) {
+        res.status(500).json({
+          success: false,
+          error: "Error Occurred on Server Side",
+          message: error.message
+        });
+      }
+    }
+  );
 export default router;
