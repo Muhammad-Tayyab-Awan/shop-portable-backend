3e333ebb61a333a628efbdd69dd2d8c310441e17
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-12-24 20:19:14 +0500

    refactored order items route to use controller for retrieving order items

--- routes/orderitems.js
+++ routes/orderitems.js
@@ -1,51 +1,14 @@
 import express from "express";
 import verifyUserLogin from "../middlewares/verifyUserLogin.js";
-import { param, validationResult } from "express-validator";
-import OrderItem from "../models/orderItems.js";
+import { param } from "express-validator";
 const router = express.Router();
+import orderItemController from "../controllers/orderItemController.js";
 
 router.get(
   "/:orderId",
   verifyUserLogin,
   param("orderId").isMongoId(),
-  async (req, res) => {
-    try {
-      const result = validationResult(req);
-      if (result.isEmpty()) {
-        const orderId = req.params.orderId;
-        const orderItems = await OrderItem.find({ orderId: orderId }).populate(
-          "productId",
-          [
-            "_id",
-            "pname",
-            "description",
-            "price",
-            "category",
-            "status",
-            "stock",
-            "discount",
-            "brand"
-          ],
-          "product"
-        );
-        res.status(200).json({
-          success: true,
-          orderItems: orderItems
-        });
-      } else {
-        res.status(400).json({
-          success: false,
-          error: result.errors
-        });
-      }
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        error: "Error Occurred on Server Side",
-        message: error.message
-      });
-    }
-  }
+  orderItemController.getAllOrderItems
 );
 
 export default router;
