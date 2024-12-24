04eaff42a887d390db40fd0df056fad578b642ae
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2024-12-24 20:55:42 +0500

    refactored cart model to simplify cart item schema and removed total calculations

--- models/cart.js
+++ models/cart.js
@@ -1,28 +1,20 @@
 import mongoose, { Schema } from "mongoose";
 
-const cartItemSchema = new Schema({
-  productId: {
-    type: Schema.Types.ObjectId,
-    ref: "product",
-    required: true
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: 1
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  total: {
-    type: Number,
-    required: true,
-    default: function () {
-      return this.quantity * this.price;
+const cartItemSchema = new Schema(
+  {
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: "product",
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1
     }
-  }
-});
+  },
+  { timestamps: true }
+);
 
 const cartSchema = new Schema(
   {
@@ -31,26 +23,10 @@ const cartSchema = new Schema(
       ref: "user",
       required: true
     },
-    items: [cartItemSchema],
-    totalQuantity: {
-      type: Number,
-      required: true,
-      default: 0
-    },
-    totalPrice: {
-      type: Number,
-      required: true,
-      default: 0
-    }
+    items: [cartItemSchema]
   },
   { timestamps: true }
 );
-
-cartSchema.pre("save", function (next) {
-  this.totalQuantity = this.items.reduce((sum, item) => sum + item.quantity, 0);
-  this.totalPrice = this.items.reduce((sum, item) => sum + item.total, 0);
-  next();
-});
 
 const Cart = mongoose.model("Cart", cartSchema);
 
